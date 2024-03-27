86f1fe67a7c684785912eff886b0258ebfb5161b
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2024-03-26 20:06:10 -0400

    Update 2 3/26/2024
    Updated contact form with CAPTCHA processing

--- send-email.php
+++ send-email.php
@@ -2,7 +2,7 @@
 
 
 if(!empty($_POST)) {
-    $recipient = "This is an email address";
+    $recipient = "okunsimon3@gmail.com";
     $header = "This is the title of the email";
     $message = "This is the message of the email";
     $firstname = strip_tags($_POST["firstName"]);
@@ -16,6 +16,26 @@ if(!empty($_POST)) {
     $details = strip_tags($_POST["details"]);
 
     $message .= "This user has submitted the form {$firstname} {$lastname} {$phonenumber} {$email} {$details}";
+
+    if(isset($_POST['g-recaptcha-response'])){
+        $captcha=$_POST['g-recaptcha-response'];
+      }
+      if(!$captcha){
+        echo '<h2>Please check the the captcha form.</h2>';
+        exit;
+      }
+      $secretKey = "6Le2zKUpAAAAACxjdYTwrY1ii-yPJPvJH_N78jKd";
+      $ip = $_SERVER['REMOTE_ADDR'];
+      // post request to server
+      $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
+      $response = file_get_contents($url);
+      $responseKeys = json_decode($response,true);
+      // should return JSON with success as true
+      if($responseKeys["success"]) {
+              echo '<h2>Thanks for posting comment</h2>';
+      } else {
+              echo '<h2>You are spammer ! Get the @$%K out</h2>';
+      }
 
     mail($recipient,$header,$message);
 
